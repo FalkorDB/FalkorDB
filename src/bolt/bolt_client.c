69ecfa6bc1b9722aaca50f99af7eacec1fcd384c
Avi Avni <avi.avni@gmail.com>
2023-09-10 20:04:35 +0300

    Revert "perf"
    This reverts commit a6eeacc519c55487446937eec708f65aafb6f25f.

--- src/bolt/bolt_client.c
+++ src/bolt/bolt_client.c
@@ -13,12 +13,14 @@
 bolt_client_t *bolt_client_new
 (
 	socket_t socket,
-	RedisModuleCtx *ctx
+	RedisModuleCtx *ctx,
+	RedisModuleEventLoopFunc on_write
 ) {
 	bolt_client_t *client = rm_malloc(sizeof(bolt_client_t));
 	client->socket = socket;
 	client->state = BS_NEGOTIATION;
 	client->ctx = ctx;
+	client->on_write = on_write;
 	client->nwrite = 2;
 	client->nread = 0;
 	client->pull = false;
@@ -26,7 +28,6 @@ bolt_client_t *bolt_client_new
 	client->shutdown = false;
 	client->reset = false;
 	client->last_read_index = 0;
-	client->finished_write = false;
 	pthread_cond_init(&client->pull_condv, NULL);
 	pthread_mutex_init(&client->pull_condv_mutex, NULL);
 	return client;
@@ -500,7 +501,7 @@ void bolt_client_finish_write
 (
 	bolt_client_t *client
 ) {
-	client->finished_write = true;
+	RedisModule_EventLoopAdd(client->socket, REDISMODULE_EVENTLOOP_WRITABLE, client->on_write, client);
 }
 
 void bolt_client_send
