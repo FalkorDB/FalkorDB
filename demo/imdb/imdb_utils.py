32393d7d937b20bd41fc8c8a2f0d3ce51bc03668
swilly22 <roilipman@gmail.com>
2023-11-24 10:19:56 +0200

    imdb test refactoring

--- demo/imdb/imdb_utils.py
+++ demo/imdb/imdb_utils.py
@@ -5,54 +5,53 @@ from falkordb import Graph, Node, Edge
 
 graph_name = "imdb"
 
+def populate_graph(db, graph):
+    # check if graph already exists
+    if graph.key in db.list_graphs():
+        return
 
-def populate_graph(redis_con, redis_graph):
-	# check if graph already exists
-	if redis_con.exists(graph_name):
-		return
+    # Load movies entities
+    movies = {}
 
-	# Load movies entities
-	movies = {}
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/resources/movies.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            title  = row[0]
+            genre  = row[1]
+            votes  = int(row[2])
+            rating = float(row[3])
+            year   = int(row[4])
 
-	with open(os.path.dirname(os.path.abspath(__file__)) + '/resources/movies.csv', 'r') as f:
-		reader = csv.reader(f, delimiter=',')
-		for row in reader:
-			title = row[0]
-			genre = row[1]
-			votes = int(row[2])
-			rating = float(row[3])
-			year = int(row[4])
+            node = Node(labels="movie", properties={'title': title,
+                                                    'genre': genre,
+                                                    'votes': votes,
+                                                    'rating': rating,
+                                                    'year': year})
+            movies[title] = node
+            graph.add_node(node)
 
-			node = Node(labels="movie", properties={'title': title,
-												    'genre': genre,
-												    'votes': votes,
-												    'rating': rating,
-												    'year': year})
-			movies[title] = node
-			redis_graph.add_node(node)
+    # Load actors entities
+    actors = {}
 
-	# Load actors entities
-	actors = {}
-
-	with open(os.path.dirname(os.path.abspath(__file__)) + '/resources/actors.csv', 'r') as f:
-		reader = csv.reader(f, delimiter=',')
-		for row in reader:
-			name = row[0]
-			yearOfBirth = int(row[1])
-			movie = row[2]
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/resources/actors.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            name        = row[0]
+            yearOfBirth = int(row[1])
+            movie       = row[2]
             # All age calculations are done where 2019 is the the current year. 
-			age = 2019 - yearOfBirth
+            age         = 2019 - yearOfBirth
 
-			if name not in actors:
-				node = Node(labels="actor", properties={'name': name, 'age': age})
-				actors[name] = node
-				redis_graph.add_node(node)
+            if name not in actors:
+                node = Node(labels="actor", properties={'name': name, 'age': age})
+                actors[name] = node
+                graph.add_node(node)
 
-			if movie in movies:
-				edge = Edge(actors[name], "act", movies[movie])
-				redis_graph.add_edge(edge)
+            if movie in movies:
+                edge = Edge(actors[name], "act", movies[movie])
+                graph.add_edge(edge)
 
-    redis_graph.commit()
-    redis_graph.create_node_fulltext_index("actor", "name")
+    graph.commit()
+    graph.create_node_fulltext_index("actor", "name")
 
     return (actors, movies)
--- tests/flow/test_imdb.py
+++ tests/flow/test_imdb.py
@@ -7,24 +7,15 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../demo/imdb')
 import imdb_queries
 import imdb_utils
 
-imdb = None
-queries = None
-redis_graph = None
-
-
 class testImdbFlow(FlowTestsBase):
     def __init__(self):
         self.env, self.db = Env()
 
     def setUp(self):
-        global imdb
-        global queries
-        global redis_graph
-        self.redis_con = self.env.getConnection()
-        redis_graph = self.db.select_graph(imdb_utils.graph_name)
-        actors, movies = imdb_utils.populate_graph(self.redis_con, redis_graph)
-        imdb = imdb_queries.IMDBQueries(actors, movies)
-        queries = imdb.queries()
+        self.graph     = self.db.select_graph(imdb_utils.graph_name)
+        actors, movies = imdb_utils.populate_graph(self.db, self.graph)
+        self.imdb      = imdb_queries.IMDBQueries(actors, movies)
+        self.queries   = self.imdb.queries()
 
     def tearDown(self):
         self.env.cmd('flushall')
@@ -33,7 +24,7 @@ class testImdbFlow(FlowTestsBase):
         # Test reversed pattern query.
         reversed_query = ReversePattern().reverse_query_pattern(query)
         # print "reversed_query: %s" % reversed_query
-        actual_result = redis_graph.query(reversed_query)
+        actual_result = self.graph.query(reversed_query)
 
         # assert result set
         self.env.assertEqual(resultset.result_set, actual_result.result_set)
@@ -42,9 +33,9 @@ class testImdbFlow(FlowTestsBase):
         self._assert_equalish(resultset.run_time_ms, actual_result.run_time_ms)
 
     def test_imdb(self):
-        for q in queries:
+        for q in self.queries:
             query = q.query
-            actual_result = redis_graph.query(query)
+            actual_result = self.graph.query(query)
 
             # assert result set
             self._assert_only_expected_results_are_in_actual_results(actual_result, q)
@@ -64,24 +55,24 @@ class testImdbFlow(FlowTestsBase):
             self.env.skip()
 
         # Execute this command directly, as its response does not contain the result set that
-        # 'redis_graph.query()' expects
-        create_node_range_index(redis_graph, 'actor', 'age', sync=True)
+        # 'self.graph.query()' expects
+        create_node_range_index(self.graph, 'actor', 'age', sync=True)
 
-        q = imdb.actors_over_85_index_scan.query
-        execution_plan = str(redis_graph.explain(q))
+        q = self.imdb.actors_over_85_index_scan.query
+        execution_plan = str(self.graph.explain(q))
         self.env.assertIn('Index Scan', execution_plan)
 
-        actual_result = redis_graph.query(q)
+        actual_result = self.graph.query(q)
 
-        self.redis_con.execute_command("GRAPH.QUERY", redis_graph.name, "DROP INDEX ON :actor(age)")
+        self.graph.drop_node_range_index("actor", "age")
 
         # assert result set
         self._assert_only_expected_results_are_in_actual_results(
             actual_result,
-            imdb.actors_over_85_index_scan)
+            self.imdb.actors_over_85_index_scan)
 
         # assert query run time
-        self._assert_run_time(actual_result, imdb.actors_over_85_index_scan)
+        self._assert_run_time(actual_result, self.imdb.actors_over_85_index_scan)
 
         # assert reversed pattern.
         self.assert_reversed_pattern(q, actual_result)
@@ -94,24 +85,24 @@ class testImdbFlow(FlowTestsBase):
             self.env.skip()
 
         # Execute this command directly, as its response does not contain the result set that
-        # 'redis_graph.query()' expects
-        create_node_range_index(redis_graph, 'movie', 'year', sync=True)
+        # 'self.graph.query()' expects
+        create_node_range_index(self.graph, 'movie', 'year', sync=True)
 
-        q = imdb.eighties_movies_index_scan.query
-        execution_plan = str(redis_graph.explain(q))
+        q = self.imdb.eighties_movies_index_scan.query
+        execution_plan = str(self.graph.explain(q))
         self.env.assertIn('Index Scan', execution_plan)
 
-        actual_result = redis_graph.query(q)
+        actual_result = self.graph.query(q)
 
-        self.redis_con.execute_command("GRAPH.QUERY", redis_graph.name, "DROP INDEX ON :movie(year)")
+        self.graph.drop_node_range_index("movie", "year")
 
         # assert result set
         self._assert_only_expected_results_are_in_actual_results(
             actual_result,
-            imdb.eighties_movies_index_scan)
+            self.imdb.eighties_movies_index_scan)
 
         # assert query run time
-        self._assert_run_time(actual_result, imdb.eighties_movies_index_scan)
+        self._assert_run_time(actual_result, self.imdb.eighties_movies_index_scan)
 
         # assert reversed pattern.
         self.assert_reversed_pattern(q, actual_result)
