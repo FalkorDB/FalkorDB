217dffa73c94ccf67bf9b5b422a3349282f66d6b
Avi Avni <avi.avni@gmail.com>
2023-12-03 14:24:19 +0200

    fix env skip for sanitizer (#527)

--- tests/flow/test_ro_query.py
+++ tests/flow/test_ro_query.py
@@ -12,7 +12,7 @@ def checkSlaveSynced(env, masterConn, slaveConn, graph_name):
 class test_read_only_query(FlowTestsBase):
     def __init__(self):
         if VALGRIND or SANITIZER != "":
-            Env.skip(None) # valgrind is not working correctly with replication
+            Environment.skip(None) # valgrind is not working correctly with replication
 
         self.env, self.db = Env(useSlaves=True)
         global master_con
