2ff1098537ac8bf8fdbc473c77e5dbceb41ba4f6
AviAvni <avi.avni@gmail.com>
2023-12-26 15:31:28 +0200

    address review

--- src/procedures/proc_sp_paths.c
+++ src/procedures/proc_sp_paths.c
@@ -412,7 +412,7 @@ static void addNeighbors
 }
 
 // get numeric attribute value of an entity otherwise return default value
-static inline SIValue _get_value_or_defualt
+static inline SIValue _get_value_or_default
 (
 	GraphEntity *ge,
 	AttributeID id,
@@ -454,8 +454,8 @@ static void SPpaths_next
 			// if depth is 0 this is the source node, there is no leading edge to it.
 			// For depth > 0 for each frontier node, there is a leading edge.
 			if(depth > 0) {
-				SIValue c = _get_value_or_defualt((GraphEntity *)&frontierConnection.edge, ctx->cost_prop, SI_LongVal(1));
-				SIValue w = _get_value_or_defualt((GraphEntity *)&frontierConnection.edge, ctx->weight_prop, SI_LongVal(1));
+				SIValue c = _get_value_or_default((GraphEntity *)&frontierConnection.edge, ctx->cost_prop, SI_LongVal(1));
+				SIValue w = _get_value_or_default((GraphEntity *)&frontierConnection.edge, ctx->weight_prop, SI_LongVal(1));
 				if(p->cost + SI_GET_NUMERIC(c) <= ctx->max_cost && p->weight + SI_GET_NUMERIC(w) <= max_weight) {
 					p->cost += SI_GET_NUMERIC(c);
 					p->weight += SI_GET_NUMERIC(w);
@@ -489,8 +489,8 @@ static void SPpaths_next
 			Path_PopNode(ctx->path);
 			if(Path_EdgeCount(ctx->path)) {
 				Edge e = Path_PopEdge(ctx->path);
-				SIValue c = _get_value_or_defualt((GraphEntity *)&e, ctx->cost_prop, SI_LongVal(1));
-				SIValue w = _get_value_or_defualt((GraphEntity *)&e, ctx->weight_prop, SI_LongVal(1));
+				SIValue c = _get_value_or_default((GraphEntity *)&e, ctx->cost_prop, SI_LongVal(1));
+				SIValue w = _get_value_or_default((GraphEntity *)&e, ctx->weight_prop, SI_LongVal(1));
 				p->cost -= SI_GET_NUMERIC(c);
 				p->weight -= SI_GET_NUMERIC(w);
 			}
