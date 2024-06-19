ba1a3166b9b055593a4a9ed1194943fb72e09d63
Emily Matheys <emilymatt96@gmail.com>
2024-06-19 10:52:15 +0300

    Update yaml and launch script

--- tests/benchmarks/run_benches.py
+++ tests/benchmarks/run_benches.py
@@ -16,13 +16,16 @@ def run_single_benchmark(idx, file_stream):
     db_image = os.getenv("DB_IMAGE")
     if not db_image:
         if "docker_image" in data:
-            db_image = data["db_image"]
+            db_image = data["docker_image"]
         else:
             db_image = "falkordb/falkordb:edge"  # If nothing is specified, use the latest image
 
     process = subprocess.Popen(["./falkordb-benchmark-go", "--yaml_config", f"./{idx}.yml",
                                 "--output_file", f"{idx}-results.json", "--override_image", db_image],
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    for line in process.stdout:
+        print(line, end='')
 
     # Wait for the process to finish and get the return code
     process.wait()
@@ -61,10 +64,10 @@ def run_single_benchmark(idx, file_stream):
 
 
 def main():
-    print("Starting benchmark suite...")
+    print("Starting benchmark suite...\n")
 
     for idx in range(20):
-        print(f"Benchmark {idx}/19:\n")
+        print(f"========== Benchmark {idx}/19 Started ================\n")
 
         if os.path.exists(f"{idx}-results.json"):
             os.remove(f"{idx}-results.json")
@@ -75,7 +78,9 @@ def main():
         with open(f"{idx}.yml", "r") as current_bench_file:
             run_single_benchmark(idx, current_bench_file)
 
-        print(f"\nBenchmark {idx}/19 Completed\n")
+        print("")
+        print(f"========== Benchmark {idx}/19 Completed ==============")
+        print("")
 
         try:
             subprocess.check_output("pidof redis-server", shell=True)
