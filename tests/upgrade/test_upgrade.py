6960f403dedc6fa00034d7c075a1ea51186be8e7
Dudi Zimberknopf <zimber.dudi@gmail.com>
2024-11-18 13:25:46 +0200

    add network

--- tests/upgrade/test_upgrade.py
+++ tests/upgrade/test_upgrade.py
@@ -2,12 +2,13 @@ import time
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../flow/')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../flow/")
 
 from common import *
 from graph_utils import *
 from random_graph import *
 from falkordb import FalkorDB
+
 
 # starts db using docker
 def run_db(image):
@@ -21,19 +22,22 @@ def run_db(image):
 
     # Run the FalkorDB container
     container = client.containers.run(
-        image,                            # Image
-        detach=True,                      # Run container in the background
-        ports={'6379/tcp': random_port},  # Map port 6379
+        image,  # Image
+        detach=True,  # Run container in the background
+        ports={"6379/tcp": random_port},  # Map port 6379
+        network=os.getenv("DOCKER_NETWORK", None),  # Use host network
     )
 
     return container, random_port
+
 
 # stop and remove docker container
 def stop_db(container):
     container.stop()
     container.remove()
 
-class test_upgrade():
+
+class test_upgrade:
     def __init__(self):
         self.env, self.replica_db = Env()
         self.replica_conn = self.env.getConnection()
@@ -48,7 +52,7 @@ class test_upgrade():
         master_db = FalkorDB(port=master_port)
 
         # Select the social graph
-        master_graph = master_db.select_graph('upgrade')
+        master_graph = master_db.select_graph("upgrade")
 
         # Generate random graph
         nodes, edges = create_random_schema()
@@ -61,11 +65,11 @@ class test_upgrade():
         # Wait for replica to sync
         master_db.connection.wait(1, 0)
 
-        replica_graph = self.replica_db.select_graph('upgrade')
+        replica_graph = self.replica_db.select_graph("upgrade")
 
         # Validate that both DBs are the same
-        
-        master_db.config_set("RESULTSET_SIZE", -1) # unlimited result-set size
+
+        master_db.config_set("RESULTSET_SIZE", -1)  # unlimited result-set size
 
         if not graph_eq(replica_graph, master_graph):
             self.env.assertTrue(graph_eq(replica_graph, master_graph))
@@ -76,10 +80,9 @@ class test_upgrade():
         self.replica_conn.replicaof("NO", "ONE")
 
     def test_v14_upgrade(self):
-        image = 'falkordb/falkordb:v4.0.7'
+        image = "falkordb/falkordb:v4.0.7"
         self.upgrade(image)
 
     def test_v15_upgrade(self):
-        image = 'falkordb/falkordb:v4.2.2'
+        image = "falkordb/falkordb:v4.2.2"
         self.upgrade(image)
-
