7582dd7d688bbd8fd68c98cd668ff9bbb934cf1f
Roi Lipman <roilipman@gmail.com>
2024-11-26 15:03:39 +0200

    added property path

---
+++ src/property_path.c
@@ -0,0 +1,254 @@
+/*
+ * Copyright FalkorDB Ltd. 2023 - present
+ * Licensed under the Server Side Public License v1 (SSPLv1).
+ */
+
+#include "RG.h"
+#include "property_path.h"
+
+// create a new empty property path
+PropertyPath *PropertyPath_new(void) {
+	PropertyPath *path;
+
+	path = rm_malloc(sizeof(PropertyPath));
+
+	path->types    = NULL;
+	path->elements = NULL;
+	path->size     = 0;
+
+	return path;
+}
+
+// returns the length of the path
+unsigned int PropertyPath_len
+(
+	const PropertyPath *path
+) {
+	ASSERT(path != NULL);
+
+	return path->size;
+}
+
+//------------------------------------------------------------------------------
+// PropertyPath getters
+//------------------------------------------------------------------------------
+
+// get path element at specified position
+// returns false if element doesn't exists, true otherwise
+bool PropertyPath_getElement
+(
+	const PropertyPathElementType *t,  // [output] [optional] element type
+	const void *v,                     // [output] [optional] element value
+	const PropertyPath *path,          // path object
+	unsigned int i                     // element position
+) {
+	ASSERT(path != NULL);
+
+	// make sure i is within bounds
+	if(i > path->size) {
+		return false;
+	}
+
+	if(t != NULL) {
+		*t = path->types[i];
+	}
+
+	if(v != NULL) {
+		switch(path->types[i]) {
+			case PATH_ELEMENT_INDEX:
+			case PATH_ELEMENT_PROPERTY_ID:
+				*v = path->elements[i].index;
+				break;
+			case PATH_ELEMENT_PROPERTY:
+				*v = path->elements[i].property;
+				break;
+			case default:
+				ASSERT(false && "unknown PropertyPathElementType");
+				break;
+		}
+	}
+
+	return true;
+}
+
+// get a property element from position i
+void PropertyPath_getProperty
+(
+	const char *v,             // [output] element value
+	const PropertyPath *path,  // path object
+	unsigned int i             // element position
+) {
+	// validations
+	ASSERT(v              != NULL);
+	ASSERT(i              <= path->size);             // i must be within bounds
+	ASSERT(path           != NULL);
+	ASSERT(path->types[i] == PATH_ELEMENT_PROPERTY);  // assert element type
+
+	*v = path->elements[i].property;
+}
+
+// get a property id element from position i
+void PropertyPath_getPropertyID
+(
+	const AttributeID *v,      // [output] element value
+	const PropertyPath *path,  // path object
+	unsigned int i             // element position
+) {
+	// validations
+	ASSERT(v              != NULL);
+	ASSERT(i              <= path->size);                // i must be within bounds
+	ASSERT(path           != NULL);
+	ASSERT(path->types[i] == PATH_ELEMENT_PROPERTY_ID);  // assert element type
+
+	*v = path->elements[i].index;
+}
+
+// get a array subscript element from position i
+void PropertyPath_getArrayIdx
+(
+	const unsigned int *v,     // [output] element value
+	const PropertyPath *path,  // path object
+	unsigned int i             // element position
+) {
+	// validations
+	ASSERT(v              != NULL);
+	ASSERT(i              <= path->size);          // i must be within bounds
+	ASSERT(path           != NULL);
+	ASSERT(path->types[i] == PATH_ELEMENT_INDEX);  // assert element type
+
+	*v = path->elements[i].index;
+}
+
+//------------------------------------------------------------------------------
+// PropertyPath construction
+//------------------------------------------------------------------------------
+
+// grow property path by `n` elements
+static void _PropertyPath_grow
+(
+	PropertyPath *path
+) {
+	ASSERT(n > 0);
+	ASSERT(path != NULL);
+
+	// make room for new element
+	path->elements = rm_realloc(path->types,
+			sizeof(PropertyPathElement) * path->size + 1);
+
+	path->types = rm_realloc(path->types,
+			sizeof(PropertyPathElementType) * path->size + 1);
+
+	// increase element count
+	path->size++;
+}
+
+// appends property access to the end of the path
+void PropertyPath_addProperty
+(
+	PropertyPath *path,  // path to append to
+	const char *property  // property to add
+) {
+	// validations
+	ASSERT(path     != NULL);
+	ASSERT(property != NULL);
+
+	_PropertyPath_grow(path, 1);
+
+	// set new element
+	path->types[path->size-1]             = PATH_ELEMENT_PROPERTY;
+	path->elements[path->size-1].property = property;
+}
+
+// appends property ID access to the end of the path
+void PropertyPath_addPropertyID
+(
+	PropertyPath **path,  // path to append to
+	AttributeID id        // property ID to add
+) {
+	// validations
+	ASSERT(path     != NULL);
+	ASSERT(property != NULL);
+
+	_PropertyPath_grow(path, 1);
+
+	// set new element
+	path->types[path->size-1]    = PATH_ELEMENT_PROPERTY_ID;
+	path->elements[path->size-1].index = id;
+}
+
+// appends array subscript access to the end of the path
+void PropertyPath_addArrayIdx
+(
+	PropertyPath **path,  // path to append to
+	unsigned int idx      // property ID to add
+) {
+	// validations
+	ASSERT(path     != NULL);
+	ASSERT(property != NULL);
+
+	_PropertyPath_grow(path, 1);
+
+	// set new element
+	path->types[path->size-1]    = PATH_ELEMENT_INDEX;
+	path->elements[path->size-1].index = idx;
+}
+
+// update an existing element
+void PropertyPath_updateElement
+(
+	PropertyPath *path,         // path to update
+	unsigned int i,             // element index to update
+	PropertyPathElementType t,  // new element type
+	PropertyPathElement v       // new element value
+) {
+	ASSERT(i           < path->size);
+	ASSERT(path        != NULL);
+	ASSERT(path->types != t);
+
+	path->types[i]   = t;
+	path->element[i] = v;
+}
+
+//------------------------------------------------------------------------------
+// PropertyPath debug
+//------------------------------------------------------------------------------
+
+// prints path to stdout
+void PropertyPath_print
+(
+	const PropertyPath *path  // path to print
+) {
+	ASSERT(path != NULL);
+
+	for(unsigned int i = 0; i < path->size) {
+		switch(path->types[i]) {
+			case PATH_ELEMENT_INDEX:
+				printf("[%d]", path->elements[i].index);
+				break;
+			case PATH_ELEMENT_PROPERTY_ID:
+				printf("._%d_", path->elements[i].index);
+				break;
+			case PATH_ELEMENT_PROPERTY:
+				printf(".%s", path->elements[i].property);
+				break;
+			case default:
+				ASSERT(false && "unknown PropertyPathElementType");
+				break;
+		}
+	}
+	printf("\r\n");
+}
+
+// free a property path
+void PropertyPath_free
+(
+	PropertyPath *path
+) {
+	ASSERT(path != NULL);
+	
+	if(path->types != NULL)    rm_free(path->types);
+	if(path->elements != NULL) rm_free(path->elements);
+
+	rm_free(path);
+}
+
---
+++ src/property_path.h
@@ -0,0 +1,146 @@
+/*
+ * Copyright FalkorDB Ltd. 2023 - present
+ * Licensed under the Server Side Public License v1 (SSPLv1).
+ */
+
+#pragma once
+
+// property path defines an access path to either a node or edge attribute-set
+// as well as a dictionary
+// e.g.
+//
+// N.flight[1].duration.minutes
+//
+// Where 'N' is the container we're accessing (Node/Edge/Map)
+// 'flight[1].duration.minutes' is the access path
+//
+// a path is constructed out of attribute and array sub-scripts
+// in the above example 'flight' referes to an attribute access of 'N'
+// while '[1]' is accessing the second element in the 'N.flight' array
+
+
+// PropertyPathElementType defines the different path element types
+typedef enum {
+	PATH_ELEMENT_PROPERTY_ID = 1,  // access a property by its ID
+	PATH_ELEMENT_PROPERTY    = 2,  // access a property by its name
+	PATH_ELEMENT_INDEX       = 4,  // access an array by index
+} PropertyPathElementType;
+
+// PropertyPathElement a element on the path
+typedef union PropertyPathElement {
+	char *property;      // property name
+	unsigned int index;  // either a property ID or an array index
+};
+
+// PropertyPath defines an access path e.g. `flight[1].duration.minutes`
+// by breaking the path into its individual elements:
+// 1. flight
+// 2. [1]
+// 3. duration
+// 4. minutes
+typedef struct {
+	PropertyPathElementType *types;  // type of each element
+	PropertyPathElement *elements;   // array of path elements
+	unsigned int size;               // number of elements in the path
+} PropertyPath;
+
+// create a new empty property path
+PropertyPath *PropertyPath_new(void);
+
+// returns the length of the path
+unsigned int PropertyPath_len
+(
+	const PropertyPath *path
+);
+
+//------------------------------------------------------------------------------
+// PropertyPath getters
+//------------------------------------------------------------------------------
+
+// get path element at specified position
+// returns false if element doesn't exists, true otherwise
+bool PropertyPath_getElement
+(
+	const PropertyPathElementType *t,  // [output] [optional] element type
+	const void *v,                     // [output] [optional] element value
+	const PropertyPath *path,          // path object
+	unsigned int i                     // element position
+);
+
+// get a property element from position i
+// return false if element doesn't exists, true otherwise
+void PropertyPath_getProperty
+(
+	const char *v,             // [output] element value
+	const PropertyPath *path,  // path object
+	unsigned int i                   // element position
+);
+
+// get a property id element from position i
+// return false if element doesn't exists, true otherwise
+void PropertyPath_getPropertyID
+(
+	const AttributeID *v,      // [output] element value
+	const PropertyPath *path,  // path object
+	unsigned int i                   // element position
+);
+
+// get a array subscript element from position i
+// return false if element doesn't exists, true otherwise
+void PropertyPath_getArrayIdx
+(
+	const unsigned int *v,     // [output] element value
+	const PropertyPath *path,  // path object
+	unsigned int i             // element position
+);
+
+//------------------------------------------------------------------------------
+// PropertyPath construction
+//------------------------------------------------------------------------------
+
+// appends property access to the end of the path
+void PropertyPath_addProperty
+(
+	PropertyPath *path,   // path to append to
+	const char *property  // property to add
+);
+
+// appends property ID access to the end of the path
+void PropertyPath_addPropertyID
+(
+	PropertyPath *path,   // path to append to
+	AttributeID id        // property ID to add
+);
+
+// appends array subscript access to the end of the path
+void PropertyPath_addArrayIdx
+(
+	PropertyPath *path,   // path to append to
+	unsigned int idx      // property ID to add
+);
+
+// update an existing element
+void PropertyPath_updateElement
+(
+	PropertyPath *path,         // path to update
+	unsigned int i,             // element index to update
+	PropertyPathElementType t,  // new element type
+	PropertyPathElement v       // new element value
+);
+
+//------------------------------------------------------------------------------
+// PropertyPath debug
+//------------------------------------------------------------------------------
+
+// prints path to stdout
+void PropertyPath_print
+(
+	const PropertyPath *path  // path to print
+);
+
+// free a property path
+void PropertyPath_free
+(
+	PropertyPath *path
+);
+
