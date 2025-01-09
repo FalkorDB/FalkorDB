6a886e151fe017c44964e99017c58888b6922da7
Avi Avni <avi.avni@gmail.com>
2025-01-09 13:01:40 +0200

    implement merge maps

--- src/value.c
+++ src/value.c
@@ -498,10 +498,26 @@ static SIValue SIValue_ConcatList(const SIValue a, const SIValue b) {
 	return resultArray;
 }
 
+// assumption: either a or b is a map
+static SIValue SIValue_MergeMap(const SIValue a, const SIValue b) {
+	SIValue result = Map_Clone(a);
+
+	// Merge b into result
+	uint bLen = Map_KeyCount(b);
+	for(uint i = 0; i < bLen; i++) {
+		SIValue key, value;
+		Map_GetIdx(b, i, &key, &value);
+		Map_Add(&result, key, value);
+	}
+
+	return result;
+}
+
 SIValue SIValue_Add(const SIValue a, const SIValue b) {
 	if(a.type == T_NULL || b.type == T_NULL) return SI_NullVal();
 	if(a.type == T_ARRAY || b.type == T_ARRAY) return SIValue_ConcatList(a, b);
 	if(a.type == T_STRING || b.type == T_STRING) return SIValue_ConcatString(a, b);
+	if(a.type == T_MAP || b.type == T_MAP) return SIValue_MergeMap(a, b);
 	/* Only construct an integer return if both operands are integers. */
 	if(a.type & b.type & T_INT64) {
 		return SI_LongVal(a.longval + b.longval);
