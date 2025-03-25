3f173c8808ab60c1ac2ca1a43a590da74c0b9d85
Avi Avni <avi.avni@gmail.com>
2025-03-25 14:23:17 +0200

    fix memory issue

--- src/arithmetic/entity_funcs/entity_funcs.c
+++ src/arithmetic/entity_funcs/entity_funcs.c
@@ -279,8 +279,9 @@ SIValue AR_PROPERTY(SIValue *argv, int argc, void *private_data) {
 		if(value->allocation == M_DISK) {
 			char node_key[ROCKSDB_KEY_SIZE];
 			RocksDB_set_key(node_key, ENTITY_GET_ID(graph_entity), prop_idx);
-			SIValue res = SI_DuplicateStringVal(RocksDB_get(node_key));
-			free(value);
+			char *str = RocksDB_get(node_key);
+			SIValue res = SI_DuplicateStringVal(str);
+			free(str);
 			return res;
 		}
 		return SI_ConstValue(value);
