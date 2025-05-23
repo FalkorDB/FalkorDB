6b6fd40f277da9c9afabe633f86047d2899fae6d
Gabriel Gomez <gomezg@tamu.edu>
2025-05-23 08:55:51 -0500

    Initial MSF code

---
+++ src/procedures/proc_msf.c
@@ -0,0 +1,424 @@
+/*
+ * Copyright FalkorDB Ltd. 2023 - present
+ * Licensed under the Server Side Public License v1 (SSPLv1).
+ */
+
+#include "RG.h"
+#include "LAGraphX.h"
+#include "GraphBLAS.h"
+
+#include "proc_msf.h"
+#include "../value.h"
+#include "../util/arr.h"
+#include "../query_ctx.h"
+#include "../util/rmalloc.h"
+#include "../datatypes/map.h"
+#include "../datatypes/array.h"
+#include "./utility/internal.h"
+#include "../graph/graphcontext.h"
+
+// CALL algo.msf({}) YIELD node, score
+// CALL algo.msf(NULL) YIELD node, score
+// CALL algo.msf({nodeLabels: ['L', 'P']}) YIELD node, score
+// CALL algo.msf({relationshipTypes: ['R', 'E']}) YIELD node, score
+// CALL algo.msf({nodeLabels: ['L'], relationshipTypes: ['E']}) YIELD node, score
+// CALL algo.msf({nodeLabels: ['L'], samplingSize:20, samplingSeed: 10})
+
+typedef struct {
+	Graph *g;              	// graph
+	GrB_Matrix tree;		// The MSF
+	GrB_Vector nodes; 	   	// nodes participating in computation
+	int *relationIDs;       // edge type(s) to traverse.
+	int relationCount;      // length of relationIDs.
+	GrB_Info info;         	// iterator state
+	GxB_Iterator it;       	// iterator
+	Node node;             	// node
+	AttributeID weight_prop;// weight attribute id
+	SIValue output[3];     	// array with up to 2 entries [node, score]
+	SIValue *yield_node;   	// nodes 
+	SIValue *yield_edge;   	// edges
+	SIValue *yield_weight; 	// edge weights
+} MSF_Context;
+
+// process procedure yield
+static void _process_yield
+(
+	MSF_Context *ctx,
+	const char **yield
+) {
+	int idx = 0;
+	for(uint i = 0; i < array_len(yield); i++) {
+		if(strcasecmp("node", yield[i]) == 0) {
+			ctx->yield_node = ctx->output + idx;
+			idx++;
+			continue;
+		}
+
+		if(strcasecmp("edge", yield[i]) == 0) {
+			ctx->yield_edge = ctx->output + idx;
+			idx++;
+			continue;
+		}
+
+		if(strcasecmp("weight", yield[i]) == 0) {
+			ctx->yield_weight = ctx->output + idx;
+			idx++;
+			continue;
+		}
+	}
+}
+
+// TODO show return enum rather than bool?
+// process procedure configuration argument
+static bool _read_config
+(
+	SIValue config,         // procedure configuration
+	LabelID **lbls,         // [output] labels
+	RelationID **rels,      // [output] relationships
+	AttributeID *weightAtt,// [output] relationship used as weight
+	bool *maxSF 			// [output] true if maximum spanning forest
+) {
+	// expecting configuration to be a map
+	ASSERT(lbls            != NULL);
+	ASSERT(rels            != NULL);
+	ASSERT(weightAtt      != NULL);
+	ASSERT(maxSF   	       != NULL);
+	ASSERT(SI_TYPE(config) == T_MAP);
+
+	// set outputs to NULL
+	*lbls = NULL;
+	*rels = NULL;
+
+	uint match_fields = 0;
+	uint n = Map_KeyCount(config);
+	if(n > 4) {
+		// error config contains unknown key
+		ErrorCtx_SetError("invalid msf configuration");
+		return false;
+	}
+
+	SIValue v;
+	LabelID *_lbls    = NULL;
+	GraphContext *gc  = QueryCtx_GetGraphCtx();
+	RelationID *_rels = NULL;
+
+	if(MAP_GETCASEINSENSITIVE(config, "nodeLabels", v)) {
+		if(SI_TYPE(v) != T_ARRAY) {
+			ErrorCtx_SetError("msf configuration, 'nodeLabels' should be an array of strings");
+			goto error;
+		}
+
+		if(!SIArray_AllOfType(v, T_STRING)) {
+			// error
+			ErrorCtx_SetError("msf configuration, 'nodeLabels' should be an array of strings");
+			goto error;
+		}
+
+		_lbls = array_new(LabelID, 0);
+		u_int32_t l = SIArray_Length(v);
+		for(u_int32_t i = 0; i < l; i++) {
+			SIValue lbl = SIArray_Get(v, i);
+			const char *label = lbl.stringval;
+			Schema *s = GraphContext_GetSchema(gc, label, SCHEMA_NODE);
+			if(s == NULL) {
+				ErrorCtx_SetError("msf configuration, unknown label %s", label);
+				goto error;
+			}
+
+			LabelID lbl_id = Schema_GetID(s);
+			array_append(_lbls, lbl_id);
+		}
+		*lbls = _lbls;
+
+		match_fields++;
+	}
+
+	if(MAP_GETCASEINSENSITIVE(config, "relationshipTypes", v)) {
+		if(SI_TYPE(v) != T_ARRAY) {
+			ErrorCtx_SetError("msf configuration, 'relationshipTypes' should be an array of strings");
+			goto error;
+		}
+
+		if(!SIArray_AllOfType(v, T_STRING)) {
+			ErrorCtx_SetError("msf configuration, 'relationshipTypes' should be an array of strings");
+			goto error;
+		}
+
+		_rels = array_new(RelationID, 0);
+		u_int32_t l = SIArray_Length(v);
+		for(u_int32_t i = 0; i < l; i++) {
+			SIValue rel = SIArray_Get(v, i);
+			const char *relation = rel.stringval;
+			Schema *s = GraphContext_GetSchema(gc, relation, SCHEMA_EDGE);
+			if(s == NULL) {
+				ErrorCtx_SetError("msf configuration, unknown relationship-type %s", relation);
+				goto error;
+			}
+
+			RelationID rel_id = Schema_GetID(s);
+			array_append(_rels, rel_id);
+		}
+		*rels = _rels;
+
+		match_fields++;
+	}
+	if(MAP_GETCASEINSENSITIVE(config, "weightRelationship", v)) {
+		if(SI_TYPE(v) != T_STRING) {
+			ErrorCtx_SetError("msf configuration, 'weightRelationship' should be a string");
+			goto error;
+		}
+
+		const char *relation = v.stringval;
+		*weightAtt = GraphContext_GetAttributeID(gc, relation);
+		if(*weightAtt == ATTRIBUTE_ID_NONE) {
+			ErrorCtx_SetError("msf configuration, unknown attribute-type %s", relation);
+			goto error;
+		}
+		match_fields++;
+	}
+	if(MAP_GETCASEINSENSITIVE(config, "objective", v)) {
+		if(SI_TYPE(v) != T_STRING) {
+			ErrorCtx_SetError("msf configuration, 'objective' should be a string");
+			goto error;
+		}
+		const char *objective = v.stringval;
+		if(strcasecmp(objective, "minimum") == 0)
+			*maxSF = false;
+		else if(strcasecmp(objective, "maximum") == 0)
+			*maxSF = true;
+		else{
+			ErrorCtx_SetError("msf configuration, unknown objective %s", objective);
+			goto error;
+		}
+	}
+
+	if(n != match_fields) {
+		ErrorCtx_SetError("msf configuration contains unknown key");
+		goto error;
+	}
+
+	return true;
+
+error:
+	if(_lbls != NULL) {
+		array_free(_lbls);
+		*lbls = NULL;
+	}
+
+	if(_rels != NULL) {
+		array_free(_rels);
+		*rels = NULL;
+	}
+
+	return false;
+}
+
+// TODO: allow for weighted inputs
+
+// invoke the procedure
+ProcedureResult Proc_MSFInvoke
+(
+	ProcedureCtx *ctx,    // procedure context
+	const SIValue *args,  // procedure arguments
+	const char **yield    // procedure outputs
+) {
+	// expecting a single argument
+
+	size_t l = array_len((SIValue *)args);
+
+	if(l > 1) return PROCEDURE_ERR;
+
+	SIValue config;
+
+	if(SIValue_IsNull(args[0])) {
+		config = SI_Map(0);
+	} else {
+		config = SI_CloneValue(args[0]);
+	}
+
+	// arg0 can be either a map or NULL
+	SIType t = SI_TYPE(config);
+	if(t != T_MAP) {
+		SIValue_Free(config);
+
+		ErrorCtx_SetError("invalid argument to algo.betweenness");
+		return PROCEDURE_ERR;
+	}
+
+	// read betweenness invoke configuration
+	// {
+	//	nodeLabels: ['A', 'B'],
+	//	relationshipTypes: ['R'],
+	//	weightRelationship: 'R',
+	//	objective: 'minimize'
+	// }
+
+	LabelID    *lbls      = NULL;
+	RelationID *rels      = NULL;
+	AttributeID weightAtt = GRAPH_UNKNOWN_RELATION;
+	bool 		maxSF 	  = false;
+
+	//--------------------------------------------------------------------------
+	// load configuration map
+	//--------------------------------------------------------------------------
+
+	bool config_ok = _read_config(config, &lbls, &rels, &weightAtt, &maxSF);
+
+	SIValue_Free(config);
+
+	if(!config_ok) {
+		return PROCEDURE_ERR;
+	}
+
+
+	//--------------------------------------------------------------------------
+	// setup procedure context
+	//--------------------------------------------------------------------------
+
+	Graph *g = QueryCtx_GetGraph();
+	MSF_Context *pdata = rm_calloc(1, sizeof(MSF_Context));
+
+	pdata->g = g;
+
+	_process_yield(pdata, yield);
+
+	// save private data
+	ctx->privateData = pdata;
+
+	GrB_Matrix A;
+	GrB_Info info;
+
+	//makes into a symetric matrix
+	info = Build_Matrix(&A, &pdata->nodes, g, lbls, array_len(lbls), rels,
+			array_len(rels), true, true);
+	ASSERT(info == GrB_SUCCESS);
+
+	// free build matrix inputs
+	if(lbls != NULL) array_free(lbls);
+	if(rels != NULL) array_free(rels);
+
+	//--------------------------------------------------------------------------
+	// run betweeness centrality
+	//--------------------------------------------------------------------------
+
+	char msg[LAGRAPH_MSG_LEN];
+	// execute Minimum Spanning Forest
+	GrB_Info msf_res =
+		LAGraph_msf(&pdata->tree, A, false, msg);
+	if(msf_res != GrB_SUCCESS) {
+		GrB_free(&A);
+		return PROCEDURE_ERR;
+	}
+	
+	// clean up algorithm inputs
+	info = GrB_free(&A);
+	ASSERT(info == GrB_SUCCESS);
+
+	
+
+	//--------------------------------------------------------------------------
+	// initialize iterator
+	//--------------------------------------------------------------------------
+
+	info = GxB_Iterator_new(&pdata->it);
+	ASSERT(info == GrB_SUCCESS);
+
+	// // iterate over participating nodes
+	// info = GxB_Vector_Iterator_attach(pdata->it, pdata->nodes, NULL);
+	// ASSERT(info == GrB_SUCCESS);
+	// iterate over spanning tree
+	info = GxB_Matrix_Iterator_attach(pdata->it, pdata->tree, NULL);
+	ASSERT(info == GrB_SUCCESS);
+    pdata->info = GxB_Matrix_Iterator_seek(pdata->it, 0);
+	return PROCEDURE_OK;
+}
+
+// yield edge and its weight.
+// yields NULL if there are no additional edges to return
+SIValue *Proc_MSFStep
+(
+	ProcedureCtx *ctx  // procedure context
+) {
+	ASSERT(ctx->privateData != NULL);
+
+	MSF_Context *pdata = (MSF_Context *)ctx->privateData;
+
+	// depleted
+	if(pdata->info == GxB_EXHAUSTED) {
+		return NULL;
+	}
+
+	// retrieve node from graph
+	GrB_Index node_i, node_j;
+	GxB_Matrix_Iterator_getIndex(pdata->it, &node_i, &node_j);
+	Edge *edge = array_new(Edge, 1);
+	// Scan relation types specified by query
+	for(uint i = 0; i < pdata->relationCount; ++i)
+	{
+		// TODO: check that this handles all relations properly 
+		Graph_GetEdgesConnectingNodes(
+			pdata->g, node_i, node_j, pdata->relationIDs[i], &edge);
+		// TODO: is this the right way to handle the symmetry?
+		Graph_GetEdgesConnectingNodes(
+			pdata->g, node_j, node_i, pdata->relationIDs[i], &edge);
+	}
+	// TODO: there may be multiple edges. which one (or should they all) be 
+	// returned?
+	// If weighted: presumably the one with the lowest weight should be returned
+	
+	// prep for next call to Proc_BetweennessStep
+	pdata->info = GxB_Matrix_Iterator_next(pdata->it);
+
+	//--------------------------------------------------------------------------
+	// set outputs
+	//--------------------------------------------------------------------------
+
+	if(pdata->yield_edge) {
+		*pdata->yield_edge = SI_Edge(edge);
+	}
+
+	if(pdata->yield_weight) {
+		*pdata->yield_weight = SI_LongVal(1); //SI_GET_NUMERIC(w);
+	}
+	return pdata->output;
+}
+
+ProcedureResult Proc_MSFFree
+(
+	ProcedureCtx *ctx
+) {
+	// clean up
+	if(ctx->privateData != NULL) {
+		MSF_Context *pdata = ctx->privateData;
+
+		if(pdata->tree		 != NULL) GrB_free(&pdata->tree);
+		if(pdata->it         != NULL) GrB_free(&pdata->it);
+		if(pdata->nodes      != NULL) GrB_free(&pdata->nodes);
+		rm_free(ctx->privateData);
+	}
+	return PROCEDURE_OK;
+}
+
+// CALL algo.msf({nodeLabels: ['Person'], relationshipTypes: ['KNOWS'],
+// samplingSize:2000, samplingSeed: 10}) YIELD node, score
+ProcedureCtx *Proc_msfCtx(void) {
+	void *privateData = NULL;
+
+	ProcedureOutput *outputs         = array_new(ProcedureOutput, 2);
+	ProcedureOutput output_node      = {.name = "node", .type = T_NODE};
+	ProcedureOutput output_component = {.name = "score", .type = T_DOUBLE};
+
+	array_append(outputs, output_node);
+	array_append(outputs, output_component);
+
+	ProcedureCtx *ctx = ProcCtxNew("algo.betweenness",
+								   1,
+								   outputs,
+								   Proc_MSFStep,
+								   Proc_MSFInvoke,
+								   Proc_MSFFree,
+								   privateData,
+								   true);
+	return ctx;
+}
+
