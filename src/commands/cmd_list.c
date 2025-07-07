4f1b7239076a506575fee143b42dc3ed0eeefb7d
Roi Lipman <roilipman@gmail.com>
2025-07-07 21:01:37 +0300

    [WIP] regex graph.list

--- src/commands/cmd_list.c
+++ src/commands/cmd_list.c
@@ -9,72 +9,98 @@
 #include "../redismodule.h"
 #include "../graph/graphcontext.h"
 #include "../deps/oniguruma/src/oniguruma.h"
+
+#include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
 
 // matches a string against a regex pattern
 // returns true if the string matches the pattern, false otherwise
-static bool _stringmatch_regex(const regex_t *regex, const char *string, int stringLen) {
-    if(regex == NULL || string == NULL) return true; // No pattern: always match
-
-    int result = onig_match(
-        regex, 
-        (const UChar *)string, 
-        (const UChar *)(string + stringLen), 
-        (const UChar *)string, 
-        NULL, 
-        ONIG_OPTION_DEFAULT
-    );
-
-    return (result >= 0);
+static bool _stringmatch_regex
+(
+	regex_t *regex,  // regex pattern
+	const char *s,   // string to match
+	int l            // string length
+) {
+    return onig_match(regex, (const UChar *)s, (const UChar *)(s + l),
+			(const UChar *)s, NULL, ONIG_OPTION_DEFAULT) >= 0;
 }
 
+// list graphs in DB
 int Graph_List
 (
-	RedisModuleCtx *ctx,
-	RedisModuleString **argv,
-	int argc
+	RedisModuleCtx *ctx,       // redis module context
+	RedisModuleString **argv,  // command arguments
+	int argc                   // number of arguments
 ) {
 	ASSERT(ctx != NULL);
 
-	if(argc != 1 && argc != 2) {
+	// expecting up to 1 argument: command name + optional argument
+	if(argc > 2) {
 		return RedisModule_WrongArity(ctx);
 	}
 
-	const char *pattern = NULL;
 	regex_t *regex = NULL;
+
+	// pattern specified
 	if(argc == 2) {
-		pattern = RedisModule_StringPtrLen(argv[1], NULL);
-		if(pattern) {
-			OnigErrorInfo einfo;
-			int rv = onig_new(&regex, (const UChar *)pattern, (const UChar *)(pattern + strlen(pattern)), ONIG_OPTION_DEFAULT, ONIG_ENCODING_UTF8, ONIG_SYNTAX_JAVA, &einfo);
-			if(rv != ONIG_NORMAL) {
-				regex = NULL; // fallback: no match
-			}
+		OnigErrorInfo einfo;
+		const char *pattern = RedisModule_StringPtrLen(argv[1], NULL);
+
+		// try to compile the regex expression
+		int rv = onig_new(&regex, (const UChar *)pattern,
+				(const UChar *)(pattern + strlen(pattern)), ONIG_OPTION_DEFAULT,
+				ONIG_ENCODING_UTF8, ONIG_SYNTAX_JAVA, &einfo);
+
+		// failed to compile regex
+		// reply with error
+		if(rv != ONIG_NORMAL) {
+			char err_msg[ONIG_MAX_ERROR_MESSAGE_LEN];
+			onig_error_code_to_str((UChar* )err_msg, rv, &einfo);
+			RedisModule_ReplyWithError(ctx, err_msg);
+
+			onig_free(regex);
+			return REDISMODULE_OK;
 		}
+
+		ASSERT(regex != NULL);
 	}
 
+	//--------------------------------------------------------------------------
+	// compose response
+	//--------------------------------------------------------------------------
+
+	// iterate over each graph in the keyspace
 	KeySpaceGraphIterator it;
 	Globals_ScanGraphs(&it);
 	RedisModule_ReplyWithArray(ctx, REDISMODULE_POSTPONED_LEN);
 
 	// reply with each graph name
-	uint64_t     n   = 0;
-	GraphContext *gc = NULL;
+	uint64_t      n  = 0;     // result-set size
+	GraphContext *gc = NULL;  // current graph
 
 	while((gc = GraphIterator_Next(&it)) != NULL) {
 		const char *name = GraphContext_GetName(gc);
         size_t len = strlen(name);
-		if(_stringmatch_regex(regex, name, len)) {
+
+		// match graph name against the regex pattern if specified
+		if(regex == NULL || _stringmatch_regex(regex, name, len)) {
 			RedisModule_ReplyWithStringBuffer(ctx, name, len);
 			n++;
 		}
+
+		// dec graph's ref count
 		GraphContext_DecreaseRefCount(gc);
 	}
 
-	if(regex) onig_free(regex);
+	// update result-set size
 	RedisModule_ReplySetArrayLength(ctx, n);
+
+	// clean up
+	if(regex != NULL) {
+		onig_free(regex);
+	}
+
 	return REDISMODULE_OK;
 }
 
