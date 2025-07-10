7416f76fb36daba1300e4415ebae5ea0d05c926a
Gabriel Gomez <gomezg@tamu.edu>
2025-07-10 15:19:20 -0500

    get_sub_weight_matrix function and testing

--- src/procedures/utility/internal.h
+++ src/procedures/utility/internal.h
@@ -45,3 +45,33 @@ GrB_Info get_sub_adjecency_matrix
 	bool symmetric           // build a symmetric matrix
 ) ;
 
+typedef enum{
+	BWM_MIN,  // choose the minimum Edge 
+	BWM_MAX   // choose the maximum Edge
+} BWM_reduce;
+
+// compose multiple label & relation matrices into a single matrix
+// L = L0 U L1 U ... Lm
+// A = L * (R0 + R1 + ... Rn) * L 
+//
+// if a weight attribute is specified, this function will pick which Edge to 
+// return given a BWM_reduce strategy. For example, BWM_MIN returns the edge 
+// with minimum weight.
+// 
+// A_w = [Attribute values of A]
+// rows = L's main diagonal
+// in case no labels are specified rows is a dense 1 vector: [1,1,...1]
+GrB_Info get_sub_weight_matrix
+(
+	GrB_Matrix *A,             // [output] matrix (EdgeIDs)
+	GrB_Matrix *A_w,           // [output] matrix (weights)
+	GrB_Vector *rows,          // [output] filtered rows
+	const Graph *g,            // graph
+	const LabelID *lbls,       // [optional] labels to consider
+	unsigned short n_lbls,     // number of labels
+	const RelationID *rels,    // [optional] relationships to consider
+	unsigned short n_rels,     // number of relationships
+	const AttributeID weight,  // weight attribute to consider
+	BWM_reduce strategy,       // use either maximum or minimum weight
+	bool symmetric             // build a symmetric matrix
+) ;
