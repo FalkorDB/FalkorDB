33826c99021a8a3e55db653d2736f8e03dce2ab8
Gabriel Gomez <gomezg@tamu.edu>
2025-07-25 20:19:53 -0500

    remove degree changes from this branch

--- src/procedures/procedure.c
+++ src/procedures/procedure.c
@@ -40,12 +40,10 @@ void Proc_Register() {
 	// register graph algorithms
 	_procRegister("algo.BFS",              Proc_BFS_Ctx);
 	_procRegister("algo.WCC",              Proc_WCCCtx);
-	_procRegister("algo.degree",           Proc_DegreeCtx);
 	_procRegister("algo.pageRank",         Proc_PagerankCtx);
 	_procRegister("algo.SPpaths",          Proc_SPpathCtx);
 	_procRegister("algo.SSpaths",          Proc_SSpathCtx);
 	_procRegister("algo.betweenness",      Proc_BetweennessCtx);
-	_procRegister("algo.degreeWeight",     Proc_DegreeWeightCtx);
 	_procRegister("algo.labelPropagation", Proc_CDLPCtx);
 
 	// register FullText Search generator
