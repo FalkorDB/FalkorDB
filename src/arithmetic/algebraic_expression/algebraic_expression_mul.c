a674eb70575f31f3072e1ddd9fbdef48570e8c53
Gabriel Gomez <gomezg@tamu.edu>
2025-08-13 09:03:16 -0500

    fixed assert failure in mult

--- src/arithmetic/algebraic_expression/algebraic_expression_mul.c
+++ src/arithmetic/algebraic_expression/algebraic_expression_mul.c
@@ -45,7 +45,11 @@ Delta_Matrix _Eval_Mul
 	GrB_Type             ty;
 	UNUSED(info) ;
 
-	ASSERT(Delta_Matrix_Synced(res));
+	// multiplication will work if there are deletions, but not if there are 
+	// additions TODO: mxm could be made to work with additions
+	GrB_OK (GrB_Matrix_nvals(&nvals, DELTA_MATRIX_DELTA_PLUS(res)));
+	ASSERT(nvals == 0);
+
 	GrB_Matrix    res_m        = Delta_Matrix_M(res);
 	GrB_Matrix    A            = NULL;
 	bool          res_modified = false;
@@ -68,8 +72,7 @@ Delta_Matrix _Eval_Mul
 
 		Delta_Matrix_type(&ty, M);
 		semiring = (ty == GrB_BOOL)? GrB_LOR_LAND_SEMIRING_BOOL: any_alive;
-		info = Delta_mxm_identity(res_m, semiring, A, M);
-		ASSERT(info == GrB_SUCCESS);
+		GrB_OK (Delta_mxm_identity(res_m, semiring, A, M));
 
 		// info = Delta_mxm_count(res_m, GxB_PLUS_PAIR_UINT64, A, M);
 		// ASSERT(info == GrB_SUCCESS);
@@ -80,25 +83,22 @@ Delta_Matrix _Eval_Mul
 
 		// exit early if 'res' is empty 0 * A = 0
 		bool alive = false;
-		info = GrB_Matrix_reduce_BOOL(
-			&alive, NULL, GrB_LOR_MONOID_BOOL, res_m, NULL);
-		ASSERT(info == GrB_SUCCESS) ;
+		GrB_OK (GrB_Matrix_reduce_BOOL(
+			&alive, NULL, GrB_LOR_MONOID_BOOL, res_m, NULL));
 		if(!alive) break ;
 	}
 
 	if(!res_modified) {
 		// copy A into res_m
-		info = GrB_transpose(res_m, NULL, NULL, A, GrB_DESC_T0) ;
-		ASSERT(info == GrB_SUCCESS) ;
+		GrB_OK (GrB_transpose(res_m, NULL, NULL, A, GrB_DESC_T0)) ;
 	}
 
 	if(res_modified)
 	{
 		GrB_Matrix res_dm = DELTA_MATRIX_DELTA_MINUS(res);
 		//add any explicit zeros to the DM matrix
-		info = GrB_Matrix_select_BOOL(
-			res_dm, NULL, NULL, GrB_VALUEEQ_BOOL, res_m, BOOL_ZOMBIE, NULL);
-		ASSERT(info == GrB_SUCCESS) ;
+		GrB_OK (GrB_Matrix_select_BOOL(
+			res_dm, NULL, NULL, GrB_VALUEEQ_BOOL, res_m, BOOL_ZOMBIE, NULL));
 		Delta_Matrix_wait(res, false);
 	}
 
