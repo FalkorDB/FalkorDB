39cc65d0d5b735fcd94b900c8de78a0d8dd82c83
Gabriel Gomez <gomezg@tamu.edu>
2025-08-19 09:57:27 -0500

    update/fix benchmark for mxm

--- tests/unit_benchmarks/benchmark_mxm.cpp
+++ tests/unit_benchmarks/benchmark_mxm.cpp
@@ -22,7 +22,6 @@ void rg_teardown(const benchmark::State &state) {
 }
 
 static void BM_mxm_all_V1(benchmark::State &state) {
-    
     GrB_Matrix   A_M   = NULL;
     Delta_Matrix A     = NULL;
     Delta_Matrix B     = NULL;
@@ -30,27 +29,25 @@ static void BM_mxm_all_V1(benchmark::State &state) {
     uint64_t     n     = 10000000;
     uint64_t     seed  = 870713428976ul;    
 
-    GrB_OK(Delta_Matrix_new(&C, GrB_BOOL, n, n, false));
     GrB_OK(Delta_Matrix_new(&A, GrB_BOOL, n, n, false));
-    GrB_OK(GrB_Matrix_new(&A_M, GrB_BOOL, n, n));
+    GrB_OK(Delta_Matrix_new(&C, GrB_BOOL, n, n, false));
 
     int additions = state.range(0);
     int deletions = state.range(1);
-    double add_density = additions / ((double) n * (double) n);
-    double del_density = deletions / ((double) n * (double) n);
+    double add_density = 1e-14 * additions;
+    double del_density = 1e-14 * deletions; 
 
     LAGraph_Random_Matrix(&A_M, GrB_BOOL, n, n, 5E-7, seed, NULL);
     Delta_Random_Matrix(&B, GrB_BOOL, n, 5E-7, add_density, del_density, seed + 1);
+
+    GrB_OK(Delta_Matrix_setM(A, A_M));
+    A_M = NULL;
     Delta_Matrix_wait(B, false);
-    Delta_Matrix_new(&A, GrB_BOOL, n, n, false);
-    Delta_Matrix_setM(A, A_M);
-    GrB_Global_set_INT32(GrB_GLOBAL, true, GxB_BURBLE);
 
     for (auto _ : state) {
         Delta_mxm(C, GxB_ANY_PAIR_BOOL, A, B);
     }
 
-    GrB_Matrix_free(&A_M);
     Delta_Matrix_free(&A);
     Delta_Matrix_free(&B);
     Delta_Matrix_free(&C);
@@ -58,38 +55,41 @@ static void BM_mxm_all_V1(benchmark::State &state) {
 
 // simulate matching 
 static void BM_mxm_chain_V1(benchmark::State &state) {
-    Delta_Matrix A = NULL;
-    Delta_Matrix C = NULL;
+    Delta_Matrix A     = NULL;
+    Delta_Matrix C     = NULL;
+    GrB_Matrix   C_M   = NULL;
+    GrB_Matrix   C_cpy = NULL;
     uint64_t     n     = 10000000;
     uint64_t     m     = 16;
     uint64_t     seed  = 870713428976ul;    
 
-    GrB_OK(Delta_Matrix_new(&C, GrB_BOOL, m, n, false));
-
+    GrB_OK(GrB_Matrix_new(&C_cpy, GrB_BOOL, m, n));
     for(int i = 0; i < m; i++){
-        Delta_Matrix_setElement_BOOL(C, i, i);
+        GrB_Matrix_setElement_BOOL(C_cpy, true, i, i);
     }
+    GrB_OK(GrB_Matrix_wait(C_cpy, GrB_MATERIALIZE));
+    GrB_OK (Delta_Matrix_new(&C, GrB_BOOL, m, n, false));
 
     int additions = state.range(0);
     int deletions = state.range(1);
     double add_density = additions / ((double) n * (double) n);
-    double del_density = deletions / ((double) n * (double) n);
-
-    Delta_Matrix_wait(C, true);
+    double del_density = deletions / ((double) n * (double) n); 
 
     Delta_Random_Matrix(&A, GrB_BOOL, n, 5E-7, add_density, del_density, seed);
-    GrB_Global_set_INT32(GrB_GLOBAL, true, GxB_BURBLE);
+    GrB_Global_set_INT32(GrB_GLOBAL, false, GxB_BURBLE);
+    // Delta_Matrix_print(A, GxB_SUMMARY);
 
     for (auto _ : state) {
+        state.PauseTiming();
+        GrB_OK(GrB_Matrix_dup(&C_M, C_cpy));
+        Delta_Matrix_clear(C);
+        Delta_Matrix_setM(C, C_M);
+        C_M = NULL;
+        state.ResumeTiming();
+
         for(int i = 0; i < 5; i++) {
             Delta_mxm(C, GxB_ANY_PAIR_BOOL, C, A);
         }
-        state.PauseTiming();
-        Delta_Matrix_clear(C);
-        for(int i = 0; i < m; i++){
-            Delta_Matrix_setElement_BOOL(C, i, i);
-        }
-        Delta_Matrix_wait(C, true);
     }
 
     Delta_Matrix_free(&A);
@@ -97,7 +97,13 @@ static void BM_mxm_chain_V1(benchmark::State &state) {
 }
 
 BENCHMARK(BM_mxm_all_V1)->Setup(rg_setup)->Teardown(rg_teardown)
-    ->Unit(benchmark::kMicrosecond)->Args({10000, 10000})->Iterations(1);
+    // ->Unit(benchmark::kMicrosecond)->Args({10000, 10000});
+    ->Unit(benchmark::kMillisecond)->Args({0, 0})->Args({10000, 10000})
+    ->Args({0, 10000})->Args({10000, 0})->Args({100, 100})->Args({0, 100})
+    ->Args({100, 0});
 BENCHMARK(BM_mxm_chain_V1)->Setup(rg_setup)->Teardown(rg_teardown)
-    ->Unit(benchmark::kMicrosecond)->Args({10000, 10000})->Iterations(1);
+    // ->Unit(benchmark::kMicrosecond)->Args({10000, 10000})->Threads(1);
+    ->Unit(benchmark::kMillisecond)->Args({0, 0})->Args({10000, 10000})
+    ->Args({0, 10000})->Args({10000, 0})->Args({100, 100})->Args({0, 100})
+    ->Args({100, 0});
 BENCHMARK_MAIN();
