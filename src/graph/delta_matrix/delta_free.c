1380807b3e446efb349038ef6b17421249967640
Gabriel Gomez <133552260+GomezGab@users.noreply.github.com>
2025-10-18 05:19:37 -0500

    Delta Matrix C source code (#1290)
    * initial Delta Matrix source code
    
    * remove rust submodule
    
    * formatting and updating iterator
    
    * Rabbit comments and basic unit tests
    
    * ai comments and matrix validation
    
    * patch leak and fix transpose checking
    
    * Change dup logic. Make delta validate validate sparcity.
    
    * docs and remove extract for boolean matricies
    
    * bot comments and compatability checks
    
    * Fix missing semicolon in delta_utils.c
    
    * Fix assertions for matrix sparsity checks
    
    ---------
    
    Co-authored-by: Roi Lipman <swilly22@users.noreply.github.com>

---
+++ src/graph/delta_matrix/delta_free.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright FalkorDB Ltd. 2023 - present
+ * Licensed under the Server Side Public License v1 (SSPLv1).
+ */
+
+#include "RG.h"
+#include "delta_matrix.h"
+#include "../../util/arr.h"
+#include "../../util/rmalloc.h"
+#include "../entities/graph_entity.h"
+
+
+
+// free RG_Matrix's internal matrices:
+// M, delta-plus, delta-minus and transpose
+void Delta_Matrix_free
+(
+	Delta_Matrix *C
+) {
+	ASSERT(C != NULL);
+	Delta_Matrix M = *C;
+	if(M == NULL) return;
+
+	if(DELTA_MATRIX_MAINTAIN_TRANSPOSE(M)){
+		Delta_Matrix T = M->transposed;
+		M->transposed = NULL;
+		GrB_OK (GrB_Matrix_free(&T->matrix));
+		GrB_OK (GrB_Matrix_free(&T->delta_plus));
+		GrB_OK (GrB_Matrix_free(&T->delta_minus));
+		rm_free(T);
+	} 
+
+	GrB_OK (GrB_Matrix_free(&M->matrix));
+	GrB_OK (GrB_Matrix_free(&M->delta_plus));
+	GrB_OK (GrB_Matrix_free(&M->delta_minus));
+
+	pthread_mutex_destroy(&M->mutex);
+
+	rm_free(M);
+	
+	*C = NULL;
+}
+
