31c792ae45fe725ae2c2086d720ffd37e1b5e9e5
GomezGab <gomezg@tamu.edu>
2026-03-17 09:51:08 -0500

    shortcut Syncronize on read queries

--- src/graph/graphcontext.h
+++ src/graph/graphcontext.h
@@ -16,7 +16,40 @@
 #include "../serializers/encode_context.h"
 #include "../serializers/decode_context.h"
 
-typedef struct GraphContext GraphContext;
+#include <stdatomic.h>
+
+// GraphContext holds refrences to various elements of a graph object
+// it is the value sitting behind a Redis graph key
+//
+// the graph context is versioned, the version value itself is meaningless
+// it is used as a "signature" for the graph schema: (labels, relationship-types
+// and attribute set) client libraries which cache the mapping between graph
+// schema elements and their internal IDs (see COMPACT reply formatter)
+// can use the graph version to understand if the schema was modified
+// and take action accordingly
+
+typedef struct GraphContext {
+	Graph *g;                              // container for all matrices and entity properties
+	int ref_count;                         // number of active references
+	rax *attributes;                       // from strings to attribute IDs
+	pthread_rwlock_t _attribute_rwlock;    // read-write lock to protect access to the attribute maps
+	char *graph_name;                      // string associated with graph
+	char **string_mapping;                 // from attribute IDs to strings
+	Schema **node_schemas;                 // array of schemas for each node label
+	Schema **relation_schemas;             // array of schemas for each relation type
+	unsigned short index_count;            // number of indicies
+	SlowLog *slowlog;                      // slowlog associated with graph
+	QueriesLog queries_log;                // log last x executed queries
+	GraphEncodeContext *encoding_context;  // encode context of the graph
+	GraphDecodeContext *decoding_context;  // decode context of the graph
+	Cache *cache;                          // global cache of execution plans
+	XXH32_hash_t version;                  // graph version
+	RedisModuleString *telemetry_stream;   // telemetry stream name
+	
+	atomic_bool write_in_progress;         // write query in progess
+	atomic_uint_fast64_t write_txn_id;     // monotonic write transaction ID
+	CircularBuffer pending_write_queue;    // pending write queries queue
+} GraphContext;
 
 //------------------------------------------------------------------------------
 // GraphContext API
@@ -73,6 +106,16 @@ void GraphContext_LockForCommit
 void GraphContext_UnlockCommit
 (
 	RedisModuleCtx *ctx,
+	GraphContext *gc
+);
+
+uint64_t GraphContext_GetWriteTransactionID
+(
+	const GraphContext *gc
+);
+
+uint64_t GraphContext_AdvanceWriteTransactionID
+(
 	GraphContext *gc
 );
 
@@ -417,4 +460,3 @@ QueriesLog GraphContext_GetQueriesLog
 (
 	GraphContext *gc
 );
-
