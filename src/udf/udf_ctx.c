9e1668db1bb2f5bd4449000da5d366e1e328fefd
Roi Lipman <roilipman@gmail.com>
2026-05-28 15:17:37 +0300

    address pr comments

--- src/udf/udf_ctx.c
+++ src/udf/udf_ctx.c
@@ -191,8 +191,8 @@ JSContext *UDFCtx_GetJSContext(void) {
 
 // make sure the UDF context is up to date
 void UDFCtx_Update(void) {
-	// quick return if there are no UDF libraries
-	if (UDF_RepoLibsCount () == 0) {
+	// quick return if UDF repo is at its initial version
+	if (UDF_RepoGetVersion () == 0) {
 		return ;
 	}
 
