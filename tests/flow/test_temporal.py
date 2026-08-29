e86c4f819fcb512341b193f09f67efd92808e19a
tsiwt <shxzhaosr@163.com>
2026-08-29 14:05:12 +0800

    test: add ordinal date boundary and time-component regression cases

--- tests/flow/test_temporal.py
+++ tests/flow/test_temporal.py
@@ -120,6 +120,8 @@ class testTemporalDate(FlowTestsBase):
           #('2015W30',    '2015-07-20'),
           ('2015-07',    '2015-07-01'),
           ('2015-202',   '2015-07-21'),
+          ('2015-060',   '2015-03-01'),
+          ('2015-001',   '2015-01-01'),
           #('2015W302',   '2015-07-21'),
           ('2015-W30',   '2015-07-20'),
           ('20150721',   '2015-07-21'),
@@ -333,6 +335,7 @@ class testTemporalLocalDateTime(FlowTestsBase):
                 #('2025049T1234', datetime()), # Year + day-of-year + hour + minute
                 ('2025-049',            datetime(year=2025, month=2, day=18, tzinfo=timezone.utc)),
                 ('2025-202',            datetime(year=2025, month=7, day=21, tzinfo=timezone.utc)),
+                ('2015-202T21:40:32',   datetime(year=2015, month=7, day=21, hour=21, minute=40, second=32, tzinfo=timezone.utc)),
         ]
 
         query = "RETURN localdatetime($str)"
